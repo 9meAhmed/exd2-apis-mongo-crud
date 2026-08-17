3dedf232541c2d434fc20dd2aece7cf5100c126f
Noman Ahmed <nahmed@mcr-systems.co.uk>
2026-08-17 10:59:39 +0500

    mail serivce added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
+const { sendMail }  = require('../services/mailerService');
 const crypto = require('crypto');
-
 const User = require('../models/User');
 
 exports.signup = async (req, res, next) => {
     const payload = req.body;
+
+    console.log("Payload:", payload);
 
     try {
 
@@ -19,6 +21,13 @@ exports.signup = async (req, res, next) => {
         payload.password = await bcrypt.hash(payload.password, 10);
 
         const newUser = await User.create(payload);
+
+        if(!newUser) {
+            return res.status(400).json({ message: "User creation failed" });
+        }
+
+        sendMail(newUser.email, "Signup Success", "Hello world?", "<b>Hello world?</b>");
+
         res.status(201).json(newUser);
     } catch (error) {
        next(error);
@@ -35,7 +44,7 @@ exports.login = async (req, res, next) => {
         return res.status(404).json({ message: "User not found" });
     }
 
-    const isPasswordValid = await bcrypt.compare(payload.password, user.password);
+    const isPasswordValid = await bcrypt.compare(payload.password, user.password,);
     
     if(!isPasswordValid) {
         return res.status(401).json({ message: "Invalid email or password" });
@@ -85,6 +94,52 @@ exports.userProfile = async (req, res, next) => {
     return res.status(200).json({ user });
 };
 
+exports.forgetPassword = async (req, res, next) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        res.status(404).json({ message: "User not found" });
+    }
+
+    user.resetPasswordCode = generateResetPasswordCode();  // Generate a random 6-character code
+    user.resetPasswordCodeExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes from now
+    await user.save();
+
+    sendMail(user.email, "Password Reset Code", `Your password reset code is: ${user.resetPasswordCode}`, `<b>Your password reset code is: ${user.resetPasswordCode}</b>`);
+    res.status(200).json({ message: "Password reset code sent to email" });
+};
+
+exports.resetPassword = async (req, res, next) => {
+    const { email, resetPasswordCode, newPassword } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    if(user.resetPasswordCode !== resetPasswordCode || user.resetPasswordCodeExpiry < new Date()) {
+        return res.status(400).json({ message: "Invalid or expired reset password code" });
+    }
+
+    const isOldPassword = await bcrypt.compare(newPassword, user.password);
+
+    // if(isOldPassword) {
+    //     return res.status(400).json({ message: "New password cannot be the same as the old password" });
+    // }
+
+    user.password = await bcrypt.hash(newPassword, 10);
+    user.resetPasswordCode = null;
+    user.resetPasswordCodeExpiry = null;
+    await user.save();
+
+    sendMail(user.email, "Password Reset Successful", "Your password has been reset successfully.", "<b>Your password has been reset successfully.</b>");
+
+    res.status(200).json({ message: "Password reset successful" });
+};
+
 const generateUserAccessToken = (user) => {
     return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_TOKEN_SECRET, { expiresIn: process.env.TOKEN_EXPIRATION || '5m' });
 }
@@ -111,3 +166,7 @@ const clearUserRefreshTokenHash = async (user) => {
     user.refreshTokenExpiry = null;
     await user.save();
 }
+
+const generateResetPasswordCode = () => {
+    return Math.random().toString(36).substring(2, 8).toUpperCase(); // Generate a random 6-character code
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,8 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.get("/user-info", authorize(), authController.userProfile);
 router.post("/refresh-token", authController.refreshToken);
+router.post("/forget-password", authController.forgetPassword);
+router.post("/reset-password", authController.resetPassword);
 
 
 module.exports = router;
